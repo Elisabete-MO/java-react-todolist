6f1435d8d812084b4d928da9996e61a3da784fe4
Bete.mrt <bete.mrt@gmail.com>
2023-12-22 02:56:35 -0300

    :bricks: ci: Inclusão Dockerfile

---
+++ backend/src/test/java/edu/java/toDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.java.toDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
